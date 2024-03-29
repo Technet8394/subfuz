0d3498c7246feaf053df8bb99f1c0497cffca318
Torstein Mauseth <torstein.mauseth@gmail.com>
2024-03-29 19:06:49 +0100

    Update Censys
    Changes in Censys API

--- plugins/censys/plugin.py
+++ plugins/censys/plugin.py
@@ -1,4 +1,5 @@
-from censys.search import CensysCertificates
+import censys
+from censys.search import CensysCerts
 from core.env import SIGINT_handler
 import signal
 from core.logger import Output
@@ -18,32 +19,30 @@ def execute(domain, config, **kwargs):
         Output().warn("Aborted plugin: %s" % NAME, False)
         return None
     try:
-        c = CensysCertificates(api_id=config['uid'], api_secret=config['secret'])
+        c = CensysCerts(api_id=config['uid'], api_secret=config['secret'])
         fields = [
             "parsed.names",
             "parsed.subject.common_name",
             "parsed.extensions.subject_alt_name.dns_names"
         ]
         results = []
-        for page in c.search(domain, fields, max_records=config['max_records']):
+        # Free API does not allow for the fields search.
+        # for page in c.search(domain, fields=fields, max_records=config['max_records']):
+        for page in c.search(domain, max_records=config['max_records']):
             results.append(page)
         #Flatten json to array
         list = []
-        for x in results:
-            if x.get('parsed.namesn'):
-                list += (x.get('parsed.names'))
-            
-            if x.get('parsed.subject.common_name'):
-                list += x.get('parsed.subject.common_name')
-            
-            if x.get('parsed.extensions.subject_alt_name.dns_names'):
-                list += x.get('parsed.extensions.subject_alt_name.dns_names')
-
+        for x in results[0]:
+            if x.get('names'):
+                list += x.get('names')
         subdomains = []
         for x in list:
             subdomains.append(x.lstrip('*').lstrip('.'))
         subdomains = sorted(set(subdomains))
         return subdomains
+    except censys.common.exceptions.CensysUnauthorizedException:
+        Output().warn("Incorrect Censys Credentials or API limitations", False)
+        return None
     except Exception as E:
         print (E)
         raise
